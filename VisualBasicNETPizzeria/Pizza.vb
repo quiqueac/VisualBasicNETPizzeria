e7e557d4e551740ab7b71d42b4eeeac7c0bf1be8
quiqueac <82471440+quiqueac@users.noreply.github.com>
2021-12-13 23:56:42 -0600

    Inicio

---
+++ VisualBasicNETPizzeria/Pizza.vb
@@ -0,0 +1,21 @@
+﻿Public NotInheritable Class Pizza
+    Private ReadOnly c_descripcion As String
+    Private ReadOnly c_ingredientes As ArrayList
+    Private ReadOnly c_cantidad_porciones As Byte
+
+    Public ReadOnly Property Descripcion() As String
+        Get
+            Return c_descripcion
+        End Get
+    End Property
+    Public ReadOnly Property Ingredientes() As ArrayList
+        Get
+            Return c_ingredientes
+        End Get
+    End Property
+    Public ReadOnly Property CantidadPorciones() As Byte
+        Get
+            Return c_cantidad_porciones
+        End Get
+    End Property
+End Class
---
+++ VisualBasicNETPizzeria/Producto.vb
@@ -0,0 +1,15 @@
+﻿Public MustInherit Class Producto
+    Protected c_nombre As String
+    Protected c_precio As Decimal
+
+    Public MustOverride Property Nombre
+
+    Public Overridable Property Precio() As Decimal
+        Get
+            Return c_precio
+        End Get
+        Set(value As Decimal)
+            c_precio = value
+        End Set
+    End Property
+End Class
